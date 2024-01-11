72c507ab0d7182c45bd243a373814635ca059d78
josefaidt <josef.aidt@gmail.com>
2024-01-11 15:52:55 -0800

    change tag names

--- amplify/backend.ts
+++ amplify/backend.ts
@@ -1,3 +1,4 @@
+import type { IConstruct } from 'constructs'
 import { Tags } from 'aws-cdk-lib'
 import { defineBackend } from '@aws-amplify/backend'
 import { CDKContextKey } from '@aws-amplify/platform-core'
@@ -15,10 +16,12 @@ const name = node.tryGetContext(CDKContextKey.BACKEND_NAME)
 const namespace = node.tryGetContext(CDKContextKey.BACKEND_NAMESPACE)
 const type = node.tryGetContext(CDKContextKey.DEPLOYMENT_TYPE)
 
-Tags.of(backend.auth).add('amplify:app-name', name)
-Tags.of(backend.auth).add('amplify:app-namespace', namespace)
-Tags.of(backend.auth).add('amplify:app-type', type)
+function tag(scope: IConstruct) {
+  Tags.of(scope).add('amplify:app-id', '???')
+  Tags.of(scope).add('amplify:app-name', namespace)
+  Tags.of(scope).add('amplify:backend-name', name)
+  Tags.of(scope).add('amplify:deployment-type', type)
+}
 
-Tags.of(backend.data).add('amplify:app-name', name)
-Tags.of(backend.data).add('amplify:app-namespace', namespace)
-Tags.of(backend.data).add('amplify:app-type', type)
+tag(backend.auth)
+tag(backend.data)
